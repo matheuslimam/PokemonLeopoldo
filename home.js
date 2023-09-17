6e91e4f9e8a98e58e8e98b3076f359a6608b0dc3
matheuslimam <47253147+matheuslimam@users.noreply.github.com>
2023-09-17 15:56:27 -0300

    home

--- home.js
+++ home.js
@@ -8,6 +8,48 @@ function atualizarHorario() {
 
 setInterval(atualizarHorario, 1000); // Atualiza o horário a cada segundo
 
+
+    window.onload = function() {
+        var musica = document.getElementById('musica');
+        musica.play(); // Inicia a reprodução quando a página é carregada
+        var aumentarVolume = document.getElementById('aumentarVolume');
+        var diminuirVolume = document.getElementById('diminuirVolume');
+    
+        aumentarVolume.addEventListener('click', function() {
+            if (musica.volume < 1) {
+                musica.volume += 0.1; // Aumenta o volume em 10%
+            }
+        });
+    
+        diminuirVolume.addEventListener('click', function() {
+            if (musica.volume > 0) {
+                musica.volume -= 0.1; // Diminui o volume em 10%
+            }
+        });
+    }
+    
+    window.addEventListener('keydown', function(event) {
+        // Verifica se a tecla pressionada é a tecla "H"
+        if (event.key === 'h' || event.key === 'H') {
+            // Oculta os elementos com ID "elemento1" e "elemento2"
+            var elemento1 = document.getElementById('contador');
+            var elemento2 = document.getElementById('horario');
+            var elemento3 = document.getElementById('navbar');
+            var elemento4 = document.getElementById('time');
+            var elemento5 = document.getElementById('perfil');
+            var elemento6 = document.getElementById('location');
+    
+            if (elemento1 && elemento2 ) {
+                elemento1.style.display = 'none';
+                elemento2.style.display = 'none';
+                elemento3.style.display = 'none';
+                elemento4.style.display = 'none';
+                elemento5.style.display = 'none';
+                elemento6.style.display = 'none';
+            }
+        }
+    });
+    
 // Simulação de contador de moedas
 let contadorMoedas = 0;
 document.getElementById('contadorMoedas').textContent = contadorMoedas;
