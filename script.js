95d8b82d77da6ffbfe556ac5c481cb095fe4eda8
Cesar Augusto M. C. da Silva <caugustomendes653@gmail.com>
2023-09-17 23:56:37 -0300

    Falta arrumar a tela de configurações

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('volume_slider').addEventListener('input', function() {
+    var volume = this.value;
+    console.log('Volume atual:', volume);
+    // Adicione aqui qualquer ação que deseja realizar com o valor do volume
+});
