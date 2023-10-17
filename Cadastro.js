192d610e583cab4f5e593470822f5d0175cd1bdb
matheuslimam <47253147+matheuslimam@users.noreply.github.com>
2023-10-17 19:37:06 -0300

    voltando

--- Cadastro.js
+++ Cadastro.js
@@ -1,31 +1,23 @@
 
-function sendConfirmationEmail(userEmail) {
-  emailjs.init('-8koAh7ngTpQGLFhG'); // Substitua 'seu_user_id' pelo seu user_id do EmailJS
 
-  const templateParams = {
-    to_email: userEmail // Substitua pelo endereço de e-mail do usuário
-  };
 
-  emailjs.send('service_zrt24f8', 'template_1zwlm5l', templateParams) // Substitua 'seu_service_id' e 'seu_template_id'
-    .then(function(response) {
-      console.log('E-mail de confirmação enviado com sucesso:', response);
-    }, function(error) {
-      console.error('Erro ao enviar o e-mail de confirmação:', error);
-    });
-}
+
+
 
 function handleCredentialResponse(response) {
-  const data = jwt_decode(response.credential);
-  if (response.credential) {
-    // Envie o e-mail de confirmação
-    sendConfirmationEmail(data.email); // Certifique-se de que "data.email" contenha o endereço de e-mail do usuário
+          const data = jwt_decode(response.credential)
+          if (response.credential) {
+            // Redirecionar o usuário para a página desejada
+            window.location.href = "loading.html";
+            //const redirectUrl = `Perfil.html?pic=${encodeURIComponent(data.picture)}`;
 
-    // Redirecione o usuário para a página desejada
-    //window.location.href = "loading.html";
-  }
-  console.log(data);
+            // Redirecionar o usuário para a página "loading.html" com o parâmetro de consulta
+           // window.location.href = redirectUrl;
+        }
+        console.log(data)
+          
+          
 }
-
 window.onload = function () {
   google.accounts.id.initialize({
     client_id: "288821345248-dof40lnsognoooda6b66ob8l2l9shv5p.apps.googleusercontent.com",
@@ -33,37 +25,37 @@ window.onload = function () {
   });
   google.accounts.id.renderButton(
     document.getElementById("buttonDiv"),
-    { theme: "outline", size: "large", logo_alignment: "center" }  // customization attributes
+    { theme: "outline", size: "large", logo_alignment:"center" }  // customization attributes
   );
   google.accounts.id.prompt(); // also display the One Tap dialog
-};
+}
+
+
+
 
 // Seleciona o formulário e o elemento de resultado
 const form = document.getElementById("trainer-form");
 const resultado = document.getElementById("resultado");
 
 // Reproduza a música automaticamente após o carregamento da página
-window.addEventListener('load', function () {
-  const audio = document.getElementById('pokemon-audio');
-  audio.play();
-});
+window.addEventListener('load', function() {
+    const audio = document.getElementById('pokemon-audio');
+    audio.play();
+        });
 
-// Adicione um ouvinte de evento para o formulário
+// Adiciona um ouvinte de evento para o formulário
 form.addEventListener("submit", function (e) {
-  e.preventDefault(); // Impede o envio do formulário padrão
+    e.preventDefault(); // Impede o envio do formulário padrão
 
-  // Pega os valores dos campos de entrada
-  const nome = document.getElementById("nome").value;
-  const idade = document.getElementById("idade").value;
-  const pokemon = document.getElementById("pokemon").value;
+    // Pega os valores dos campos de entrada
+    const nome = document.getElementById("nome").value;
+    const idade = document.getElementById("idade").value;
+    const pokemon = document.getElementById("pokemon").value;
 
-  // Exibe o resultado na div de resultado
-  resultado.innerHTML = `
-        <p>Nome: ${nome}</p>
+    // Exibe o resultado na div de resultado
+    resultado.innerHTML = `
+        <p>Nome: ${nome}</p> 
         <p>Idade: ${idade}</p>
         <p>Pokémon Favorito: ${pokemon}</p>
     `;
 });
-
-
-
