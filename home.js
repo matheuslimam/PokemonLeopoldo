79488eff5013ca1d0e9311e49e219f5c87caa24c
matheuslimam <47253147+matheuslimam@users.noreply.github.com>
2023-10-17 23:49:51 -0300

    tradução

--- home.js
+++ home.js
@@ -1,4 +1,12 @@
-
+//Disparado como callback para ?cb=googleTranslateElementInit
+function googleTranslateElementInit()
+{
+    new google.translate.TranslateElement({
+        pageLanguage: 'pt', //Idioma principal da página
+        includedLanguages: 'en,es', //Idiomas que pode traduzir
+        layout: google.translate.TranslateElement.InlineLayout.HORIZONTAL
+    }, 'google_translate_element'); //google_translate_element é o elemento no div
+}
 
 function atualizarHorario() {
     const data = new Date();
