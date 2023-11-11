590c887fcbc4bf8a635ca816a5346e8533e79b34
matheuslimam <47253147+matheuslimam@users.noreply.github.com>
2023-11-11 20:28:29 -0300

    comentario

--- perfil.js
+++ perfil.js
@@ -4,6 +4,7 @@ const userName = localStorage.getItem('user_name');
 const profilePicture = localStorage.getItem('user_pic');
 const userPokemon = localStorage.getItem('user_pokemon');
 
+//muda o atributos da pagina
 fullname.textContent = userName;
 userPokemon.textContent = userPokemon;
 pic.setAttribute("src", profilePicture);
