1b014af320d0e6cf5f9844d231ba28e2bb533985
matheuslimam <47253147+matheuslimam@users.noreply.github.com>
2023-11-11 20:39:35 -0300

    testando get pokemon inicial

--- perfil.js
+++ perfil.js
@@ -4,7 +4,27 @@ const userName = localStorage.getItem('user_name');
 const profilePicture = localStorage.getItem('user_pic');
 const userPokemon = localStorage.getItem('user_pokemon');
 
+function hasUserEntered(userId) {
+    return getCookie(`userPokemon_${userId}`);
+}
+
 //muda o atributos da pagina
 fullname.textContent = userName;
-userPokemon.textContent = userPokemon;
+userPokemon.textContent = hasUserEntered(userId);
 pic.setAttribute("src", profilePicture);
+
+function getCookie(name) {
+    const cname = name + "=";
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(cname) === 0) {
+            return c.substring(cname.length, c.length);
+        }
+    }
+    return "";
+}
