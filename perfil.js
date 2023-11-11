d785ccaf419c641ad1fd2a3fff1ae67d68e10e73
matheuslimam <47253147+matheuslimam@users.noreply.github.com>
2023-11-11 20:27:01 -0300

    testando pokemon

--- perfil.js
+++ perfil.js
@@ -2,6 +2,8 @@
 const userId = localStorage.getItem('user_id');
 const userName = localStorage.getItem('user_name');
 const profilePicture = localStorage.getItem('user_pic');
+const userPokemon = localStorage.getItem('user_pokemon');
 
 fullname.textContent = userName;
+userPokemon.textContent = userPokemon;
 pic.setAttribute("src", profilePicture);
