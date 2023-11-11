9ada8f38c30a61100c1f91723869abcb4ea29ca2
matheuslimam <47253147+matheuslimam@users.noreply.github.com>
2023-11-11 13:16:31 -0300

    nome do usuario

--- perfil.js
+++ perfil.js
@@ -1,5 +1,7 @@
 
-
+const userId = localStorage.getItem('user_id');
+const userName = localStorage.getItem('user_name');
 const profilePicture = localStorage.getItem('user_pic');
 
+fullname.textContent = userName;
 pic.setAttribute("src", profilePicture);
